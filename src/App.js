0a2aca35396cc88ce96442740728ed5c8c28cd90
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-07-22 00:25:19 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Todos from "./components/Todos";
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "study your ass off",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "study your ass off",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "study your ass off",
+        completed: false
+      }
+    ]
+  };
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
+}
+export default App;
